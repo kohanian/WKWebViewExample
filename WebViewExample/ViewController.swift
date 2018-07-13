52922fbbfed952604606e4889ad59cf9ab50066d
Kyle Ohanian <kyle.ohanian@willowtreeapps.com>
2018-07-13 11:28:39 -0400

    Initial Commit

---
+++ WebViewExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebViewExample
+//
+//  Created by Kyle Ohanian on 7/13/18.
+//  Copyright © 2018 Kyle Ohanian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
